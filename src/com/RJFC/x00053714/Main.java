f27c31d135ee6fe7a6f59b724d6ac5e25ccc86f2
laushahidi <00365919@uca.edu.sv>
2020-04-17 18:17:10 -0600

    Add: menu

--- src/com/RJFC/x00053714/Main.java
+++ src/com/RJFC/x00053714/Main.java
@@ -1,8 +1,51 @@
 package com.RJFC.x00053714;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        byte op = 0;
+
+        String menu = "1. Agregar empleado\n" +
+                "2. Despedir empleado\n" +
+                "3. Ver lista de empleados\n" +
+                "4. Calcular sueldo\n" +
+                "5. Mostrar totales\n" +
+                "6. Salir\n" +
+                "Su opcion: ";
+
+        do{
+            op = Byte.parseByte(JOptionPane.showInputDialog(null, menu));
+
+            switch (op){
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    JOptionPane.showMessageDialog(null, "Saliendo . . .");
+                    break;
+            }
+        } while(op != 8);
     }
-}
+
+    }
+
